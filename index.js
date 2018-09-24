4da5a7a6e257e8750d264055a516c5b8794fd067
Maxwell Morgan <maxwelltmorgan@gmail.com>
2018-09-24 04:30:15 -0600

    style updates

--- index.js
+++ index.js
@@ -15,67 +15,58 @@ let squared = document.getElementsByTagName("li");
 
 const displayController = (e) => {
     if( squared[0].textContent == "X" && squared[1].textContent == "X" && squared[2].textContent == "X"){
-        result.textContent = "you win";
+        result.textContent = player1.name + " wins";
         player1Score += 1;
     } else if( squared[3].textContent == "X" && squared[4].textContent == "X" && squared[5].textContent == "X"){
-        result.textContent = "you win";
+        result.textContent = player1.name + " wins";
         player1Score += 1;
     } else if( squared[6].textContent == "X" && squared[7].textContent == "X" && squared[8].textContent == "X"){
-        result.textContent = "you win";
+        result.textContent = player1.name + " wins";
         player1Score += 1;
     } else if( squared[0].textContent == "X" && squared[3].textContent == "X" && squared[6].textContent == "X"){
-        result.textContent = "you win";
+        result.textContent = player1.name + " wins";
         player1Score += 1;
     } else if( squared[1].textContent == "X" && squared[4].textContent == "X" && squared[7].textContent == "X"){
-        result.textContent = "you win";
+        result.textContent = player1.name + " wins";
         player1Score += 1;
     } else if( squared[2].textContent == "X" && squared[5].textContent == "X" && squared[8].textContent == "X"){
-        result.textContent = "you win";
+        result.textContent = player1.name + " wins";
         player1Score += 1;
     } else if( squared[0].textContent == "X" && squared[4].textContent == "X" && squared[8].textContent == "X"){
-        result.textContent = "you win";
+        result.textContent = player1.name + " wins";
         player1Score += 1;
     } else if( squared[2].textContent == "X" && squared[4].textContent == "X" && squared[6].textContent == "X"){
-        result.textContent = "you win";
+        result.textContent = player1.name + " wins";
         player1Score += 1;
     } else if( squared[0].textContent == "O" && squared[1].textContent == "O" && squared[2].textContent == "O"){
-        result.textContent = "you lose";
+        result.textContent = player2.name + " wins";
         player2Score += 1;
     } else if( squared[3].textContent == "O" && squared[4].textContent == "O" && squared[5].textContent == "O"){
-        result.textContent = "you lose";
+        result.textContent = player2.name + " wins";
         player2Score += 1;
     } else if( squared[6].textContent == "O" && squared[7].textContent == "O" && squared[8].textContent == "O"){
-        result.textContent = "you lose";
+        result.textContent = player2.name + " wins";
         player2Score += 1;
     } else if( squared[0].textContent == "O" && squared[3].textContent == "O" && squared[6].textContent == "O"){
-        result.textContent = "you lose";
+        result.textContent = player2.name + " wins";
         player2Score += 1;
     } else if( squared[1].textContent == "O" && squared[4].textContent == "O" && squared[7].textContent == "O"){
         result.textContent = "you lose";
         player2Score += 1;
     } else if( squared[2].textContent == "O" && squared[5].textContent == "O" && squared[8].textContent == "O"){
-        result.textContent = "you lose";
+        result.textContent = player2.name + " wins";
         player2Score += 1;
     } else if( squared[0].textContent == "O" && squared[4].textContent == "O" && squared[8].textContent == "O"){
-        result.textContent = "you lose";
+        result.textContent = player2.name + " wins";
         player2Score += 1;
     } else if( squared[2].textContent == "O" && squared[4].textContent == "O" && squared[6].textContent == "O"){
-        result.textContent = "you lose";
+        result.textContent = player2.name + " wins";
         player2Score += 1;
     } else if ( gameBoard.board.indexOf("") == -1){
-        result.textContent = "tied up";
+        result.textContent = player2.name + " wins";
     }
     p1Score.textContent = player1.name + " : " + player1Score;
     p2Score.textContent = player2.name + " : " + player2Score;
-//    squared[0].textContent = "";
-//    squared[1].textContent = "";
-//    squared[2].textContent = "";
-//    squared[3].textContent = "";
-//    squared[4].textContent = "";
-//    squared[5].textContent = "";
-//    squared[6].textContent = "";
-//    squared[7].textContent = "";
-//    squared[8].textContent = "";
 }
 
 const reset = () => {
@@ -89,6 +80,7 @@ const reset = () => {
     squared[7].textContent = "";
     squared[8].textContent = "";
     gameBoard.board = ["","","","","","","","",""];
+    result.textContent = "Game On!";
 }
 
 //factories for players
